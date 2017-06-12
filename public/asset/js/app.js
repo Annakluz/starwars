adba1058bbf5b082432bf7afc326d8432d4d9fd1
Anna karina Luz <anna.luz.duran@gmail.com>
2017-06-12 11:18:34 -0500

    planetas

--- public/asset/js/app.js
+++ public/asset/js/app.js
@@ -1,39 +1,66 @@
-var cargarPagina = function (){
-  cargarPersonajes();
+var cargarPagina = function () {
+    cargarPersonajes();
+    $(document).on("click", ".personaje", mostrarDetallePersonaje); //cuando los elementos se crean dinamicamente  se ocupa "on"
 
 };
 
 
 var cargarPersonajes = function () {
 
-var url = 'http://swapi.co/api/people/';
-$.getJSON(url, function (response) {
-  console.log(response);
-  // La diferencia con .get es que siempre se obtiene data, usualmente se utiliza para cuando no sabemos que tipo de informacion vamos a obtener.
-   // $.getJSON, solamente esperamos informacion tipo JSON. 
+    var url = 'http://swapi.co/api/people/';
+    $.getJSON(url, function (response) {
+        console.log(response);
+        // La diferencia con .get es que siempre se obtiene data, usualmente se utiliza para cuando no sabemos que tipo de informacion vamos a obtener.
+        // $.getJSON, solamente esperamos informacion tipo JSON. 
 
-  var personajes = response.results;
-  var total = response.count;
+        var personajes = response.results;
+        var total = response.count;
 
-  mostrarTotalPersonajes(total);
-  mostrarPersonajes(personajes);
-});
+        mostrarTotalPersonajes(total);
+        mostrarPersonajes(personajes);
+    });
 
 };
-var mostrarTotalPersonajes = function (total){
-  $("#total").text("total de personajes:"+total);
+var mostrarTotalPersonajes = function (total) {
+    $("#total").text("total de personajes:" + total);
 }
-var mostrarPersonajes = function(personajes) {
-  var $ul = $("#personajes");
-  personajes.forEach(function(personaje) {
-    console.log(personaje);
-    console.log(personaje.name);
-    console.log(personaje.height);
+var mostrarPersonajes = function (personajes) {
+    var $ul = $("#personajes");
+    personajes.forEach(function (personaje) {
+        console.log(personaje);
+        console.log(personaje.name);
+        console.log(personaje.height);
 
-    var $li = $("<li />");
-    $li.text(personaje.name +" "+personaje.height);
-    $ul.append($li);
+        var $li = $("<li />");
+        $li.addClass("personaje");
+        $li.attr("data-url", personaje.homeworld);
+        $li.text(personaje.name + " " + personaje.height);
 
-  });
+        $ul.append($li);
+
+    });
 }
+
+var plantillaPlaneta = "<h2>Planeta</h2>" +
+    "<p>Noimbre: __nombre__</p>" +
+    "<p>Clima:__clima__</p>";
+
+var mostrarDetallePersonaje = function () {
+   
+    var url = $(this).data("url");
+        var $planetaContenedor = $("#planeta");
+
+        $.getJSON(url, function (response) {
+               
+            
+            $planetaContenedor.html( plantillaPlaneta.replace("__nombre__", response.name)
+                    .replace("__clima__", response.climate)
+            );
+        });
+       
+
+
+};
+
+
 $(document).ready(cargarPagina);
